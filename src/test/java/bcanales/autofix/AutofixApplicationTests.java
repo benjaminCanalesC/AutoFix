53d95e7b61d112dd1e8220e85e2f38fc054434e1
benjaminCanalesC <135075287+benjaminCanalesC@users.noreply.github.com>
2024-03-27 10:34:47 -0300

    feat: base java project

---
+++ src/test/java/bcanales/autofix/AutofixApplicationTests.java
@@ -0,0 +1,13 @@
+package bcanales.autofix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
